04f541746eec5ab46c00d56f3f3efbf409b2140a
kibersashka <aleksandraveldaeva4@gmail.com>
2026-04-23 16:25:31 +0300

    add config

--- app-task-tracker/auth-service/auth-impl/src/main/java/com/task/tracker/authimpl/config/SecurityConfig.java
+++ app-task-tracker/auth-service/auth-impl/src/main/java/com/task/tracker/authimpl/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final AccountUserDetailsService userDetailsService;
@@ -33,8 +35,8 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/login",
-                                "/api/auth/register"
+                                "auth/login",
+                                "auth/register"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
