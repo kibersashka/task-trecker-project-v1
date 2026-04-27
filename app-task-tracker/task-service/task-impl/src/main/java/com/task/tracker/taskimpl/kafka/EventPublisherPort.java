12814d75f0b55070d3ec9525fd41c74dfe5826e9
kibersashka <aleksandraveldaeva4@gmail.com>
2026-04-27 22:38:03 +0300

    add task-service

---
+++ app-task-tracker/task-service/task-impl/src/main/java/com/task/tracker/taskimpl/kafka/EventPublisherPort.java
@@ -0,0 +1,23 @@
+package com.task.tracker.taskimpl.kafka;
+
+import java.util.UUID;
+
+public interface EventPublisherPort {
+
+    String TASK_LEVEL_UP_COMMAND = "task.level.up.command";
+
+    void publish(String topic, String key, Object payload);
+
+    default void publishLevelUp(
+            String jsonPayload,
+            UUID user_id
+    ) {
+
+        publish(
+                TASK_LEVEL_UP_COMMAND,
+                user_id.toString(),
+                jsonPayload
+        );
+    }
+
+}
