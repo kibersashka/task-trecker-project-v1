5a4f2639e86e59077a7cbd34627f64db26dc38de
kibersashka <aleksandraveldaeva4@gmail.com>
2026-04-27 22:37:01 +0300

    fix user-service

--- app-task-tracker/user-service/user-impl/src/main/java/com/task/tracker/userimpl/repository/AccountInfoRepository.java
+++ app-task-tracker/user-service/user-impl/src/main/java/com/task/tracker/userimpl/repository/AccountInfoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -17,4 +18,6 @@ public interface AccountInfoRepository extends JpaRepository<AccountInfo, UUID> 
     """)
     List<AccountInfo> findUsersAboveAverageXp();
 
+    Optional<AccountInfo> findAccountInfoById(UUID id);
 }
+
